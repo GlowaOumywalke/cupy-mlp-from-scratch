11f28389bd636d06c66de2f98dc5808eb331cbb8
ziutek <hgrzymkiewicz@gmail.com>
2026-02-06 22:41:08 +0100

    add project structure

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def sigmoid(x: float) -> float:
+    return 1 / (1 + np.exp(-x))
+
+
+def d_sigmoid(x: float) -> float:
+    s = sigmoid(x)
+    return s * (s - 1)
+
+
+def cross_entropy(y: np.ndarray, p: np.ndarray) -> float:
+    return -np.sum(y * np.log(p))
+
+
+def linear_comb(w: np.ndarray, x: np.ndarray) -> float:
+    return np.sum(w * x)
