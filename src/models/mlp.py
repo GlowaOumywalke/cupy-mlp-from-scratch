b5dfa88aba3801bb75cb24481090df7aad7a958e
ziutek <hgrzymkiewicz@gmail.com>
2026-02-15 22:59:30 +0100

    add backprop logic

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,26 +1,58 @@
 import numpy as np
-from src.utils import linear_comb, sigmoid, cross_entropy
 from typing import Callable
+from src.utils import d_cross_entropy, d_sigmoid, sgd, sigmoid, cross_entropy
 
 
 class MLP:
     def __init__(self) -> None:
-        self.act_func = sigmoid
-        self.loss_func = cross_entropy
+        self.act_funcs = {"sigmoid": [sigmoid, d_sigmoid]}
+        self.loss_funcs = {"cross_entropy": [cross_entropy, d_cross_entropy]}
+        self.optimizers = {"sgd": sgd}
+        self.params = {"batch_size": 1, "optimizer": "sgd", "lr": 0.001, "epochs": 100}
+        self.loss_func = None
         self.layers = []
 
-    def set_act_fun(self, f: Callable[[float], float]) -> None:
-        self.act_func = f
+    def add_layer(
+        self,
+        n_in: int,
+        n_neur: int,
+        act_func: str,
+    ) -> None:
+        if act_func not in self.act_funcs.keys():
+            raise ValueError("Invalid activation function name")
+        self.layers.append(Layer(n_in, n_neur, self.act_funcs[act_func][0]))
+        self.L_afd = self.act_funcs[act_func][1]
 
-    def set_loss_func(self, f: Callable[[float], float]) -> None:
-        self.loss_func = f
+    def configure_training(self, loss_func: str, **kwargs) -> None:
+        if loss_func not in self.loss_funcs.keys():
+            raise ValueError("Invalid loss function name")
 
-    def add_layer(self, n_in: int, n_neur: int) -> None:
-        self.layers.append(Layer(n_in, n_neur))
+        self.loss_func = self.loss_funcs[loss_func][0]
 
-    def propagate_back(self, mu: float = 0.05) -> None:
-        for layer in self.layers:
-            ...
+        for key in kwargs:
+            if key not in self.params.keys():
+                raise ValueError(f"Invalid param key: {key}")
+
+        self.params.update(kwargs)
+
+    def propagate_back(
+        self, batch_Y: np.ndarray, batch_X: np.ndarray, batch_Z: np.ndarray
+    ) -> None:
+        if not self.loss_func:
+            raise RuntimeError(
+                "Loss function not set. Use configure_training method first"
+            )
+
+        lf = self.loss_func
+        afd = self.L_afd
+        err_L = lf(batch_Y, batch_X) * afd(batch_Z)
+        opt = self.params["optimizer"]
+        lr = self.params["lr"]
+        prev_err = err_L
+
+        for layer in self.layers[::-1]:
+            # next layer error
+            next_err = (layer.weights.T @ prev_err) * afd()
 
     def train(self, x_train, y_train, x_test, y_test) -> None: ...
 
@@ -28,14 +60,16 @@ class MLP:
 
 
 class Layer:
-    def __init__(self, n_in: int, n_neur) -> None:
-        self.weights = np.random.normal(n_in, n_neur)
-        self.bias = np.random.normal(n_neur)
-        self.act_func = sigmoid
+    def __init__(
+        self,
+        n_in: int,
+        n_neur: int,
+        act_func: Callable[[np.ndarray], np.ndarray],
+    ) -> None:
+        self.weights = np.random.normal(0, 1, size=(n_neur, n_in))
+        self.bias = np.zeros(n_neur)
+        self.act_func = act_func
 
-    def set_act_fun(self, f: Callable[[float], float]) -> None:
-        self.act_func = f
-
-    def forward(self, x: np.ndarray) -> float:
-        z = linear_comb(x, self.weights) + self.weights
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        z = np.matmul(self.weights, x) + self.bias
         return self.act_func(z)
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,40 @@
 import numpy as np
 
 
-def sigmoid(x: float) -> float:
+def sigmoid(x: float | np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-x))
 
 
-def d_sigmoid(x: float) -> float:
+def d_sigmoid(x: float | np.ndarray) -> float | np.ndarray:
     s = sigmoid(x)
     return s * (s - 1)
 
 
-def cross_entropy(y: np.ndarray, p: np.ndarray) -> float:
-    return -np.sum(y * np.log(p))
+def softmax(x: np.ndarray) -> np.ndarray:
+    dem = np.sum([np.exp(xi) for xi in x])
+    return np.exp(x) / dem
 
 
-def linear_comb(w: np.ndarray, x: np.ndarray) -> float:
-    return np.dot(w, x)
+def mat_softmax(X: np.ndarray) -> np.ndarray:
+    dem = np.sum(np.exp(X), axis=1, keepdims=True)
+    return np.exp(X) / dem
+
+
+def cross_entropy(Y: np.ndarray, X: np.ndarray) -> float:
+    """
+    Y - one-hot expected, Y.shape = (batch_size, n_classes)
+    X - predictions,      X.shape = (batch_size, n_classes)
+    """
+    X = mat_softmax(X)
+    eps = 1e-10
+    X = np.clip(X, eps, 1)
+    return np.mean(-np.sum(Y * np.log(X), axis=1))
+
+
+def d_cross_entropy(Y: np.ndarray, X: np.ndarray) -> np.ndarray:
+    X = mat_softmax(X)
+    return (X - Y) / Y.shape[0]
+
+
+def sgd(X: np.ndarray, X_grad: np.ndarray, lr: float) -> np.ndarray:
+    return X - lr * X_grad
